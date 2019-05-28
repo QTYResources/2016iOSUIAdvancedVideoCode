9897ee6d01c1580266e7d723c49fd78abdb501bc
xiaotuan <qin_ty@yuntianzn.com>
2019-05-28 15:01:57 +0800

    添加第2天=>LoadCustomController->加载自定义控制器

---
+++ Day02/LoadCustomController/LoadCustomController/HMViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadCustomController
+//
+//  Created by QinTuanye on 2019/5/28.
+//  Copyright © 2019 QinTuanye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMViewController : UIViewController
+
+
+@end
+
---
+++ Day02/LoadCustomController/LoadCustomController/HMViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  LoadCustomController
+//
+//  Created by QinTuanye on 2019/5/28.
+//  Copyright © 2019 QinTuanye. All rights reserved.
+//
+
+#import "HMViewController.h"
+
+@interface HMViewController ()
+
+@end
+
+@implementation HMViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
